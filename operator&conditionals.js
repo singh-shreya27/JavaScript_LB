0da5eebf95f2bfddc4f8033b426c57193356c0c0
Shreya Singh <singh.s.shreya2004@gmail.com>
2025-01-14 19:21:44 +0530

    Update operator&conditionals.js

--- operator&conditionals.js
+++ operator&conditionals.js
@@ -1,1 +1,31 @@
+                             //Operators and Conditionals.
 
+// TYPES:
+//1.Arithmetic operators: +,-,*,/,%,**
+//Binary (+) 
+//Unary(++)
+
+//2.Comparison Operators:==(loose equality: value should be same),
+//===(strict equality : value and type of data both should be same),>,<,>=,<=,!=,!==
+
+//3.Ternary Operators:Syntax:
+condition? value1 : value2
+
+let age=5;
+let status1 =(age>18) ? 'I can vote' :'I cannot vote';
+console.log(status1);
+
+//4.Logical Operators:
+//AND(&&)
+//OR(||)
+//NOT(!) 
+
+//working with non-booleans:  e.g.(true && 'babbar')
+//falsy and truthy.
+//falsy:undefined,null,0,false,NaN,' '.
+//truthy: anything which is not truthy.
+
+//SHORT-CIRCUITING:
+//(false|| true|| false||true||false) ..after one true it does not needs to check further.
+
+//5.Bitwise-Operators:
