4929e80410b41598caf3b30e8208a4f3bc3c6bc6
Shreya Singh <singh.s.shreya2004@gmail.com>
2025-01-14 17:39:03 +0530

    Update Variables&DataTypes.js

--- Variables&DataTypes.js
+++ Variables&DataTypes.js
@@ -35,3 +35,23 @@ a=null;
                                    //const:
 //redefinition/redeclaration/reassignment not possible.
 
+           //VARIABLE NAMING CONVENTION:
+//can start with number/_/$
+// no space between
+//camelCase
+//meaningful
+//don't use reserved keywords as names
+
+//DATATYPES:
+//1.Primitive data type : number,string,boolean,undefined,null,bigInt,symbol.
+//2.Non-primitive data type. (we won't deal with them here)
+
+//Number:1,2,23,4.14,5.79,3.14 (between -(2^53 -1) to (2^53-1))
+//String: sequence of characters
+//Boolean: true/false
+//undefined: no value provided to data-type.
+let age;
+//null: null value is given to data-type.
+let value=null;
+//bigInt: numbers greater than (2^53-1)
+
