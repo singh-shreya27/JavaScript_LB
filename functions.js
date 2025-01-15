42cd3b2a2efeb09c81b528bd2f63f461748e8d8e
Shreya Singh <singh.s.shreya2004@gmail.com>
2025-01-15 19:17:51 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,17 @@
+                //Functions
+//It is a block of code which performs a specific task.
 
+//function definition
+function sayName(){
+    console.log("Love Babbar");
+}
+
+//function use - function
+sayName();
+
+function printCounting(){
+    for(let i=0;i<=100;i++){
+        console.log(i);
+    }
+}
+printCounting();
